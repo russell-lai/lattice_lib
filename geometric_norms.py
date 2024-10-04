fcc91743195b6c62693dd87991b485b9b70d56b2
Russell W. F. Lai <russell.lai@protonmail.com>
2024-10-04 12:53:43 +0300

    norms for matrices

--- geometric_norms.py
+++ geometric_norms.py
@@ -1,5 +1,5 @@
 from sage.all import *
-from sage.structure.element import Vector
+from sage.structure.element import Vector, Matrix
 
 def z_coeff(v):
     if isinstance(v, Vector):
@@ -14,13 +14,17 @@ def K_vec(f, z_coeff):
     return vector([K(z_coeff[i*phi:(i+1)*phi]) for i in range(len(z_coeff)/phi)])
     
 def coeff_norm(v, order=oo):
-    if isinstance(v, Vector):
+    if isinstance(v, Matrix):
+        return coeff_norm(vector(v), order)
+    elif isinstance(v, Vector):
         return vector([coeff_norm(x, order) for x in v]).norm(order)
     else: 
         return vector(v).norm(order).n()
 
 def canon_norm(v, order=2):
-    if isinstance(v, Vector):
+    if isinstance(v, Matrix):
+        return canon_norm(vector(v), order)
+    elif isinstance(v, Vector):
         return vector([canon_norm(x, order) for x in v]).norm(order)
     else: 
         if order == 2:
